5ef38deb2957c9444bd063d176d67ff790bca8a5
Matt Draghi <mtd5264@gmail.com>
2022-03-19 14:18:59 -0400

    commit tree 9d16a820644c3e9c5ee3e0ee7d2219410ee6b430

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else
+      item = Item.find(params[:id])
+    end
+    render json: item, include: :user
+
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.user_id = params[:user_id]
+    item.save
+    render json: item, status: 201
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
